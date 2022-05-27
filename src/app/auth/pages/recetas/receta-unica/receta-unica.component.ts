5e9d2918025efbff2bd441decaea38f609296cbf
bubita <bubfat@gmail.com>
2022-05-27 12:01:01 +0200

    Comments 6

--- src/app/auth/pages/recetas/receta-unica/receta-unica.component.ts
+++ src/app/auth/pages/recetas/receta-unica/receta-unica.component.ts
@@ -59,7 +59,7 @@ export class RecetaUnicaComponent implements OnInit {
 
   cogerComentariosPorReceta(){
     this.recetas_api.getComentariosByReceta(this.recetaId).subscribe( (res:any) =>{
-      this.comentarios.push(res[0]);
+      this.comentarios.push(res);
       console.log(this.comentarios)
       return this.comentarios;
     })
